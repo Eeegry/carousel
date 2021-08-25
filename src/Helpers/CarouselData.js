35318d6b64fefea6506601d4704625d79322c4bb
eeegry <kolesnikov6@inbox.lv>
2021-08-25 15:07:21 +0300

    Removed height and width prop-s for mobile version images. Added clip to mobile version images.

--- src/Helpers/CarouselData.js
+++ src/Helpers/CarouselData.js
@@ -1,7 +1,21 @@
 import React from "react";
 export let content = [
   <img src="src/images/city.jpg" />,
+  <img src="src/images/city.jpg" />,
+  <img src="src/images/city.jpg" />,
   <div>
-    <button>1</button>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+      pharetra viverra felis nec sodales. Proin pellentesque finibus tortor, at
+      iaculis orci elementum sit amet. Duis ullamcorper venenatis ante vel
+      mollis. Aenean pellentesque lorem eu elit sollicitudin tincidunt.
+      Vestibulum diam tortor, luctus id libero at, congue ornare elit. Integer
+      quam nisi, laoreet nec est ac, placerat egestas lectus. Praesent volutpat
+      velit vitae feugiat ornare. Ut eu viverra elit, vel iaculis sem. Nullam
+      rhoncus arcu dignissim, vehicula ligula ut, euismod neque. Pellentesque ut
+      massa ultricies, accumsan ipsum sed, auctor mi. Fusce nec justo semper,
+      feugiat velit ac, pharetra lorem. Morbi porta vulputate mi, a auctor felis
+      aliquam quis. Ut id justo sagittis, vulputate nisl id, porttitor ex.
+    </p>
   </div>,
 ];
